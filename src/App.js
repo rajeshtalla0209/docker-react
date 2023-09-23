5ee44b835771b9c7b94413acb9364bce3b3d5825
rajeshtalla0209 <rajeshtalla0209@gmail.com>
2023-09-23 10:17:05 +0530

    remved --coverage from tavis.yml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There. i have update the code
+          Hi There. i have update the code. second pull request
         </p>
         </header>
     </div>
